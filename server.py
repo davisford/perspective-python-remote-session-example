dc1e62b19f805ddee7597daf6a228f3386a1a127
Davis Ford <dford@nabancard.com>
2024-05-21 15:38:14 -0400

    working example

--- server.py
+++ server.py
@@ -10,22 +10,20 @@
 #  ┃ of the [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0). ┃
 #  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
-import os
-import os.path
-import logging
-import string
-import tornado.websocket
-import tornado.web
-import tornado.ioloop
-import threading
 import concurrent.futures
 import json
+import logging
 import random
+import string
+import threading
 
-from perspective import Table, PerspectiveManager, PerspectiveTornadoHandler
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+from perspective import PerspectiveManager, PerspectiveTornadoHandler, Table
 
 
-def perspective_thread(manager, table):
+def perspective_thread(manager):
     psp_loop = tornado.ioloop.IOLoop()
     with concurrent.futures.ThreadPoolExecutor() as executor:
         manager.set_loop_callback(psp_loop.run_in_executor, executor)
@@ -38,6 +36,8 @@ class RPCWebSocket(tornado.websocket.WebSocketHandler):
 
     def __init__(self, *args, **kwargs):
         self.manager = kwargs.pop("manager")
+        self.name = 'unitialized'
+        self.table = Table([])
         super(RPCWebSocket, self).__init__(*args, **kwargs)
 
     def open(self):
@@ -49,6 +49,7 @@ class RPCWebSocket(tornado.websocket.WebSocketHandler):
         client so that Perspective may open the `Table` virtually via the
         `open_table()` method."""
         path = json.loads(message).get("filepath")
+        print(f"load filepath {path}")
         self.name = "".join(
             random.choices(string.ascii_uppercase + string.digits, k=10)
         )
@@ -56,12 +57,26 @@ class RPCWebSocket(tornado.websocket.WebSocketHandler):
         with open(path, "rb") as f:
             self.table = Table(f.read())
             self.manager.host_table(self.name, self.table)
+            names = self.manager.get_table_names()
+            print(f"manager hosting {names}")
+            for n in names:
+                t = self.manager.get_table(n)
+                if t is not None:
+                    print(f"table {n} still exists")
+
 
         self.write_message(json.dumps({"table_name": self.name}))
 
     def on_close(self):
-        tornado.ioloop.IOLoop.current().add_timeout(1, lambda: self.table.delete())
+        tornado.ioloop.IOLoop.current().add_timeout(1, lambda: self.cleanup)
         print("WebSocket closed, table deleted")
+
+    def cleanup(self):
+        t = self.manager.get_table(self.name)
+        print(f"deleting {self.name}")
+        if t is not None:
+            t.delete()
+            print(f"deleted: {self.name}")
 
 
 def make_app():
@@ -100,7 +115,7 @@ def make_app():
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(8080)
+    app.listen(8086)
     logging.critical("Listening on http://localhost:8080")
     loop = tornado.ioloop.IOLoop.current()
     loop.start()
